4719e61f50df28894f410f16641fe042e7512057
Dragan Kovacevic <kowach93@gmail.com>
2021-10-12 18:23:03 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+ }
 }
 </script>
 
 <style>
+.pagination {
+         justify-content: center;
+         flex-wrap: wrap;
+      }
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,117 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>SpaceX Ships</h1>
+    <p class="type_text">Type: <select v-model="type">
+      <option label="All"></option>
+      <option value="Cargo">Cargo</option>
+      <option value="High Speed Craft">High Speed Craft</option>
+      <option value="Tug">Tug</option>
+      <option value="Barge">Barge</option>
+    </select></p>
+      <ApolloQuery :query="require('../graphql/Ships.gql')" :variables="{limit, type}">
+        <template v-slot="{result:{loading, error, data} }">
+          <div v-if="data">
+            <div class="data_container">
+              <div class="header">
+              <p>Picture</p>
+              <p>Name</p>
+              <p>Type</p>
+              <p>Weight kg</p>
+              <p>Year built</p>
+              <p>Weight lbs</p>
+            </div>
+            <div class="ship_container" v-for="ship in data.ships" :key="ship.id">
+              <div><img :src="ship.image"></div>
+              <p>{{ship.name}}</p>
+              <p>{{ship.type}}</p>
+              <p>{{ship.weight_kg}}</p>
+              <p>{{ship.year_built}}</p>
+              <p>{{ship.weight_lbs}}</p>
+            </div>
+            </div>
+          </div>
+        </template>
+      </ApolloQuery>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'HelloWorld',
+  data() {
+    return {
+      limit: 20,
+      type: ""
+    };
+  },
   props: {
     msg: String
-  }
+  },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+select {
+  margin-bottom: 20px;
+  height: 50px;
+  padding: 5px;
+  font-size: 20px;
+  font-weight: bold;
+  border: 2px solid orange;
+}
+
+a { cursor: pointer; }
+        .pagination {
+           justify-content: center;
+           flex-wrap: wrap;
+        }
+
+.ship_container, .header {
+  display: grid;
+  grid-template-columns: 150px 150px 150px 150px 150px 150px;
+  grid-column-gap: 50px;
+  align-items: center;
+  justify-content: space-evenly;
+  width: fit-content;
+  height: 150px;
+}
+
+.type_text {
+  font-weight: bold;
+  color: black;
+  text-align: left;
+  margin-left: 350px;
+}
+
+.ship_container {
+  background-color: #42b983;
+  border: 2px solid white;
+}
+
+.data_container {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+
+.header {
+  margin-bottom: 2px;
+  background-color: orange;
+  height: 100px;
+}
+
+img {
+  width: 150px;
+  height: 150px;
+}
+
+p {
+  font-size: 20px; 
+}
+
 h3 {
   margin: 40px 0 0;
 }
